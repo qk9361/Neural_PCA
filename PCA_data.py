d4190893904d5cfbc29e4dd8c05524218a0f0320
Qian <qian_ku@lighthill.eoc.dlr.de>
2019-10-14 17:04:39 +0200

    update for classification approch

--- PCA_data.py
+++ PCA_data.py
@@ -8,8 +8,41 @@ import torch.utils.data as Data
 from torch.nn import init
 import h5py
 import time
+import math
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+class ang_dif(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, y):
+        dif_ang = torch.empty(len(x), 1)
+        for i in range(len(x)):
+            dif_ang[i] = torch.dot(x[i,:], y[i,:]) / (torch.norm(x) * torch.norm(y))
+        dif_ang = dif_ang / math.pi * 180
+        return torch.mean(dif_ang ** 2)
+
+class judge_layer(nn.Module):
+    def __init__(self):
+        super(judge_layer, self).__init__()
+
+    def forward(self, x):
+        out = torch.empty(len(x), 4)
+        for i in range(len(x)):
+            if x[i,0] > 0 and x[i,1] > 0:
+                out[i,:] = torch.tensor([1, 0, 0, 0])
+            if x[i,0] < 0 and x[i,1] > 0:
+                out[i,:] = torch.tensor([0, 1, 0, 0])
+            if x[i,0] < 0 and x[i,1] < 0:
+                out[i,:] = torch.tensor([0, 0, 1, 0])
+            if x[i,0] > 0 and x[i,1] < 0:
+                out[i,:] = torch.tensor([0, 0, 0, 1])
+        return out
+
+def adjust_lr(optimizer, epoch, init_lr):
+    lr = init_lr ** (epoch // 10)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
 # features, labels = make_blobs(n_samples = 10000, n_features = 4, centers =[
 # [0, 0, 0, 0], [10, 10, 10, 10], [10, 20, 40, 80], [10, 30, 90, 270]], cluster_std = [0.1, 0.2, 0.2, 0.2],
 # random_state = 9)
@@ -54,29 +87,38 @@ f.close()
 # print(labels.shape, type(labels))
 
 in_feature = 2
-out_feature = 4
-hid_feature1 = 200
-hid_feature2 = 400
-hid_feature3 = 200
+out_feature = 2
+hid_feature1 = 300
+hid_feature2 = 600
+hid_feature3 = 900
+hid_feature4 = 600
+hid_feature5 = 300
 
 net = nn.Sequential(nn.Linear(in_feature, hid_feature1),
                     nn.Sigmoid(),
-                    # nn.BatchNorm1d(hid_feature1),
+                    nn.BatchNorm1d(hid_feature1),
                     nn.Linear(hid_feature1, hid_feature2),
                     nn.Sigmoid(),
-                    # nn.BatchNorm1d(hid_feature2),
+                    nn.BatchNorm1d(hid_feature2),
                     nn.Linear(hid_feature2, hid_feature3),
                     nn.Sigmoid(),
-                    # nn.BatchNorm1d(hid_feature3),
-                    nn.Linear(hid_feature3, out_feature)
+                    nn.BatchNorm1d(hid_feature3),
+                    nn.Linear(hid_feature3, hid_feature4),
+                    nn.Sigmoid(),
+                    nn.BatchNorm1d(hid_feature4),
+                    nn.Linear(hid_feature4, hid_feature5),
+                    nn.Sigmoid(),
+                    nn.BatchNorm1d(hid_feature5),
+                    nn.Linear(hid_feature5, out_feature),
+                    judge_layer()
                     )
 
 features = torch.tensor(features, dtype = torch.float)
-label = torch.tensor(labels, dtype = torch.float)
-train_set = Data.TensorDataset(features[0:80000,:], label[0:80000,:])
-test_set = Data.TensorDataset(features[80000:-1,:], label[80000:-1,:])
+label = torch.tensor(labels)
+train_set = Data.TensorDataset(features[0:3000,:], label[0:3000,:])
+test_set = Data.TensorDataset(features[3000:-1,:], label[3000:-1,:])
 
-batch_size = 200
+batch_size = 50
 num_workers = 4
 
 train_iter = Data.DataLoader(
@@ -95,35 +137,58 @@ for name, param in net.named_parameters():
     print(name, param.shape, type(param))
     init.normal_(param, mean = 0, std = 1)
 
-loss = nn.MSELoss()
+xx, yy = iter(test_iter).next()
+print(net(xx), net(xx).dtype)
+print(yy, yy.dtype)
+
+# loss = nn.MSELoss()
+# loss = ang_dif()
+loss = nn.CrossEntropyLoss()
 
 num_epochs = 50
+init_lr = 0.1
 
-optimizer = optim.Adam(net.parameters(), lr=0.001, weight_decay = 0.1)
+optimizer = optim.Adam(net.parameters(), lr=init_lr, weight_decay = 0.1)
 print(optimizer)
+
+input = torch.randn(3, 5, requires_grad=True)
+target = torch.randint(5, (3,), dtype=torch.int64)
+print(input, '\n', target)
 
 for epoch in range(1, num_epochs+1):
     net = net.to(device)
     print('training on ', device)
     batch_count = 0
     train_l_sum, n, start = 0.0, 0, time.time()
+    # adjust_lr(optimizer, epoch, init_lr)
     for x, y in train_iter:
         x = x.to(device)
         y = y.to(device)
-        y_hat = net(x)
-        l = loss(y_hat, y)
+        y_hat = net(x).to(device)
+        l = loss(y_hat, y.T).sum()
         optimizer.zero_grad()
         l.backward()
         optimizer.step()
-        # train_l_sum += l.cpu().item()
-        # n += y.shape[0]
+        train_l_sum += l.cpu().item()
+        train_acc_sum += (y_hat.argmax(dim = 1) == y).cpu().sum().item()
+        n += y.shape[0]
         batch_count += 1
     print('epoch: %d,  loss: %.4f, time: %.3f sec' %(epoch, l.cpu().item(), time.time() - start))
+    # if
+    # if abs(l.cpu().item()) < 0.005:
+    #     break
 
 xx, yy = iter(test_iter).next()
 xx = xx.to(device)
-dif = net(xx).cpu() - yy
-
-print(dif, '\n', torch.norm(dif) / len(yy))
-print(dif, '\n', torch.norm(dif), torch.norm(yy))
+dif = loss(net(xx).cpu(), yy)
 print(net(xx), '\n', yy)
+
+# print(dif)
+# # print(dif, '\n', torch.norm(dif), torch.norm(yy))
+# pred_error = torch.empty(len(xx), 1)
+# for i in range(len(xx)):
+#     pred_error[i] = torch.dot(net(xx).cpu()[i,:], yy[i,:]) / (torch.norm(net(xx).cpu()[i,:]) * torch.norm(yy[i,:]))
+# pred_error = pred_error / math.pi * 180
+# print(pred_error)
+# for name, param in net.named_parameters():
+#     print(name, '\n', param)
--- data_generate.py
+++ data_generate.py
@@ -2,33 +2,63 @@ import numpy as np
 import random
 import h5py
 import math
+from sklearn.decomposition import PCA, KernelPCA
+import numpy.linalg as LA
+import matplotlib.pyplot as plt
 
-x = np.random.normal(0, 10, 10000).T
-y = np.random.normal(0, 5, 10000).T
+x = np.random.normal(0, 10, 1000).T
+y = np.random.normal(0, 5, 1000).T
 
 features = np.stack([x, y], axis = 1)
-features0 = features
 print(features.shape, '\n', features[0:10])
 
 eig_vec = np.array([[1, 0], [0, 1]], dtype = np.float)
-print(eig_vec.shape, '\n', eig_vec)
+# ang = np.random.normal(45, 45, 10000).T
+ang = [45, 135, 225, 315]
+# rot = np.array([[math.cos(ang), -math.sin(ang)],
+#                 [math.sin(ang), math.cos(ang)]],
+#                 dtype = np.float)
 
-labels = np.tile(eig_vec.ravel(), (10000,1))
-print(labels.shape, '\n', labels)
+# eig_vec_rot = np.empty([10000, 2])
+# features_rot = np.empty([10000, 2])
+features_rot = locals()
 
-data = locals()
-
-for i in range(10):
-    ang = (1+i) * 10 / 180 * math.pi
-    rot = np.array([[math.cos(ang), -math.sin(ang)],
-                    [math.sin(ang), math.cos(ang)]],
+for i in range(len(ang)):
+    rot = np.array([[math.cos(ang[i]), -math.sin(ang[i])],
+                    [math.sin(ang[i]), math.cos(ang[i])]],
                     dtype = np.float)
-    print('rot shape ', rot.shape)
-    data['data' + str(i)] = np.dot(features0, rot)
-    print('data shape: ', data['data' + str(i)].shape)
-    labels_rot = np.tile(rot.ravel(), (10000,1))
-    features = np.vstack((features, data['data' + str(i)]))
-    labels = np.vstack((labels, labels_rot))
+    # eig_vec_rot[i,:] = rot[:,0].T
+    features_rot['features_rot'+str(i)] = np.dot(features, rot)
+
+features = features_rot0
+for i in range(1,len(ang)):
+    features = np.vstack([features, features_rot['features_rot'+str(i)]])
+
+# eig_vec_rot = np.tile(rot[:,0].ravel(), (10000,1))
+# features_rot = np.dot(features, rot)
+#
+# eig_vec_rot = np.vstack([np.tile([1,0], (10000,1)), eig_vec_rot])
+# features_rot = np.vstack([features, features_rot])
+# print(eig_vec_rot.shape)
+print(features.shape)
+class1 = np.tile([1,0,0,0], (1000,1))
+class2 = np.tile([0,1,0,0], (1000,1))
+class3 = np.tile([0,0,1,0], (1000,1))
+class4 = np.tile([0,0,0,1], (1000,1))
+labels = np.vstack([class1,class2,class3,class4])
+# data = locals()
+
+# for i in range(10):
+#     ang = (1+i) * 10 / 180 * math.pi
+#     rot = np.array([[math.cos(ang), -math.sin(ang)],
+#                     [math.sin(ang), math.cos(ang)]],
+#                     dtype = np.float)
+#     print('rot shape ', rot.shape)
+#     data['data' + str(i)] = np.dot(features0, rot)
+#     print('data shape: ', data['data' + str(i)].shape)
+#     labels_rot = np.tile(rot.ravel(), (10000,1))
+#     features = np.vstack((features, data['data' + str(i)]))
+#     labels = np.vstack((labels, labels_rot))
 
 np.random.shuffle(features)
 print('features shape: ', features.shape)
@@ -39,3 +69,28 @@ f = h5py.File('sim_data.h5', 'w')
 f.create_dataset('features', data = features)
 f.create_dataset('labels', data = labels)
 f.close()
+
+# pca = PCA(n_components = 1)
+# features_pca = pca.fit_transform(features)
+# print(pca.explained_variance_ratio_)
+# print(pca.explained_variance_)
+# print(pca.components_)
+#
+# plt.figure()
+# plt.subplot(3,1,1)
+# plt.scatter(features[:,0], features[:,1])
+# plt.subplot(3,1,2)
+# plt.plot(features_pca[:,0])
+#
+# pred = pca.fit_transform(features)
+# print(pred.shape, type(pred))
+# print(pred[0:10,:])
+#
+# kpca = KernelPCA(n_components = 1, kernel = 'cosine')
+# features_kpca = kpca.fit_transform(features)
+# plt.subplot(3,1,3)
+# plt.plot(features_kpca[:,0])
+# plt.show()
+# print(kpca.explained_variance_ratio_)
+# print(kpca.explained_variance_)
+# print(kpca.components_)
